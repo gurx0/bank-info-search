30dfb5d7c2350c324f56c86a3464aee8aae6655a
gurx0 <pankratov20062504@gmail.com>
2025-01-06 14:24:09 +0300

    + MainViewModel

---
+++ app/src/main/java/com/example/bank_info_search/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.bank_info_search.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bank_info_search.domain.BinDomainModel
+import com.example.bank_info_search.domain.useCases.GetBinDetailsUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val getBinDetailsUseCase: GetBinDetailsUseCase) : ViewModel() {
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState
+
+    fun onBinInputChange(input: String) {
+        _uiState.value = _uiState.value.copy(binInput = input)
+    }
+
+    fun fetchBinDetails() = viewModelScope.launch {
+        val details = getBinDetailsUseCase(uiState.value.binInput)
+        _uiState.value = _uiState.value.copy(binDetails = details)
+    }
+}
+
+data class MainUiState(
+    val binInput: String = "",
+    val binDetails: BinDomainModel? = null
+)
