576516958e25f7479dbaefbe46acda43124ffdb7
gurx0 <pankratov20062504@gmail.com>
2025-01-07 00:45:07 +0300

    + BinRepository

--- app/src/main/java/com/example/bank_info_search/domain/BinRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bank_info_search.domain
-
-interface BinRepository {
-    suspend fun getBinDetails(bin: String): BinDomainModel
-    suspend fun getHistory(): List<BinDomainModel>
-}
