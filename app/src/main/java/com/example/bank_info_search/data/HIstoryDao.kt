552aea2bff1b3a58861029920ff9dcfc0ae08ee0
gurx0 <pankratov20062504@gmail.com>
2025-01-06 14:32:18 +0300

    + HistoryDao

---
+++ app/src/main/java/com/example/bank_info_search/data/HIstoryDao.kt
@@ -0,0 +1,18 @@
+package com.example.bank_info_search.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.bank_info_search.data.models.HistoryItem
+
+@Dao
+interface HistoryDao {
+    @Query("SELECT * FROM history")
+    suspend fun getAllHistory(): List<HistoryItem>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertHistory(item: HistoryItem)
+}
+
+
