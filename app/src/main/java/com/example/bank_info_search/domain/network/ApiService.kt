23bdc164f46246a37ac118645760b0be55825f94
gurx0 <pankratov20062504@gmail.com>
2025-01-07 00:43:24 +0300

    change BIN model

--- app/src/main/java/com/example/bank_info_search/domain/network/ApiService.kt
+++ app/src/main/java/com/example/bank_info_search/domain/network/ApiService.kt
@@ -1,10 +1,10 @@
 package com.example.bank_info_search.domain.network
 
-import com.example.bank_info_search.data.models.BinResponse
+import com.example.bank_info_search.data.models.BinDomainModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
     @GET("/{bin}")
-    suspend fun getBinDetails(@Path("bin") bin: String): BinResponse
+    suspend fun getBinDetails(@Path("bin") bin: String): BinDomainModel
 }
