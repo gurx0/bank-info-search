8a6f5a033e47ffe646e72d13ef2b352895143354
gurx0 <pankratov20062504@gmail.com>
2025-01-07 00:51:44 +0300

    commmit

--- app/src/main/java/com/example/bank_info_search/domain/useCases/GetBinDetailsUseCase.kt
+++ app/src/main/java/com/example/bank_info_search/domain/useCases/GetBinDetailsUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.bank_info_search.domain.useCases
 
-import com.example.bank_info_search.domain.BinDomainModel
-import com.example.bank_info_search.domain.BinRepository
+import com.example.bank_info_search.data.models.BinDomainModel
+import com.example.bank_info_search.data.repo.BinRepository
 
 class GetBinDetailsUseCase(private val repository: BinRepository) {
     suspend operator fun invoke(bin: String): BinDomainModel = repository.getBinDetails(bin)
