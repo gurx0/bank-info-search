beee0192af73b1aa022cab1641857cf88621206e
gurx0 <pankratov20062504@gmail.com>
2025-01-07 00:52:42 +0300

    bug fix

--- app/src/main/java/com/example/bank_info_search/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/bank_info_search/presentation/viewmodel/MainViewModel.kt
@@ -2,7 +2,7 @@ package com.example.bank_info_search.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.bank_info_search.domain.BinDomainModel
+import com.example.bank_info_search.data.models.BinDomainModel
 import com.example.bank_info_search.domain.useCases.GetBinDetailsUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,8 +17,11 @@ class MainViewModel(private val getBinDetailsUseCase: GetBinDetailsUseCase) : Vi
     }
 
     fun fetchBinDetails() = viewModelScope.launch {
-        val details = getBinDetailsUseCase(uiState.value.binInput)
-        _uiState.value = _uiState.value.copy(binDetails = details)
+        try {
+            val details = getBinDetailsUseCase(uiState.value.binInput)
+            _uiState.value = _uiState.value.copy(binDetails = details)
+        } catch (_: Exception){
+        }
     }
 }
 
