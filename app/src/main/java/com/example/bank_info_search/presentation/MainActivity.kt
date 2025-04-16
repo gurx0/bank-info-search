1fe3d26b4ef14f0b9983b6620e1b8b5064b8a9eb
gurx0 <pankratov20062504@gmail.com>
2025-04-16 19:57:10 +0300

    implements SettingsScreen.kt

--- app/src/main/java/com/example/bank_info_search/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bank_info_search/presentation/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.bank_info_search.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import com.example.bank_info_search.domain.di.appModule
 import com.example.bank_info_search.presentation.components.AppNavigation
 import com.example.bank_info_search.presentation.viewmodel.HistoryViewModel
 import com.example.bank_info_search.presentation.viewmodel.MainViewModel
+import com.example.bank_info_search.presentation.viewmodel.ThemeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
@@ -24,11 +31,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             val mainViewModel: MainViewModel by viewModel()
             val historyViewModel: HistoryViewModel by viewModel()
+            val themeViewModel: ThemeViewModel by viewModel()
+            val isDarkTheme = themeViewModel.isDarkTheme.collectAsState()
 
-            AppNavigation(
-                mainViewModel = mainViewModel,
-                historyViewModel = historyViewModel
-            )
+            // Используем системную тему по умолчанию
+            val darkTheme = isDarkTheme.value
+            MyAppTheme(darkTheme) {
+                AppNavigation(
+                    mainViewModel = mainViewModel,
+                    historyViewModel = historyViewModel,
+                    themeViewModel = themeViewModel
+                )
+            }
         }
     }
+}
+
+@Composable
+fun MyAppTheme(
+    darkTheme: Boolean,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
 }
